1dd4abc6a8a2a4a69b8ad8ddd0622f1d78a8b7c2
Karol03 <karol_dudzic_03@wp.pl>
2019-05-08 01:06:27 +0200

    To first push

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <QCoreApplication>
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     sf::Window window(sf::VideoMode(800, 600), "My window");
+
     return a.exec();
 }
